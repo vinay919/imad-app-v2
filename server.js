62c6838718745c438baaed724fdef7f7f3fa72ab
vinay919 <vv3108@gmail.com>
2017-02-12 20:18:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ var articles={
 }
 };
 function createTemplate(data){
-    var title=data.title;
+   // var title=data.title;
     var heading=data.heading;
     var date=data.date;
     var content=data.content;
