f4be1d4fa1c64fbb49968cd102360dcec6002855
vinay919 <vv3108@gmail.com>
2017-02-13 21:48:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ element.innerHTML='New Value';
 //move the image
 var img=document.getElementById('madi');
 img.onclick=function(){
-  img.style.marginLeft='100px';
+  img.style.marginLeft='100px';  
 };
