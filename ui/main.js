0480c3ffd768f5f9ba7f5a7359b39755f20e6f84
vinay919 <vv3108@gmail.com>
2017-02-14 08:37:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,14 +31,31 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_button');
 submit.onclick=function(){
-    //make a request to the server and send name
-    
-    //capture a list of name and render it as a list
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
+    //create a request to the object
+  var request=new XMLHttpRequest();
+  
+  //capture the response and store it in a variable
+  request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE)  {
+        //Take some action
+        if(request.status===200){
+             //capture a list of name and render it as a list
+            var names=request.responseText;// ['name1','name2','name3','name4'];
+            names=JSON.parse(names);
+            var list='';
+            for(var i=0;i<names.length;i++){
+                list+='<li>'+names[i]+'</li>';
+            }
+            var ul=document.getElementById('nameList');
+            ul.innerHTML=list;
+        }
     }
-    var ul=document.getElementById('nameList');
-    ul.innerHTML=list;
+    //if not
+  };
+  
+  //Make the request
+  request.open('GET','http://vinay919.imad.hasura-app.io/submit-name?name='+name,true);
+  request.send(null);
+    
+   
 }
