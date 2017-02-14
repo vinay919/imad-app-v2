65a0f07e097bf4312c1fa1636f60930ff79571fd
vinay919 <vv3108@gmail.com>
2017-02-14 08:31:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,9 +107,10 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
     //get the name from the request
-    var name=req.params.name;
+    //var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     //JSON javascript object notation
     
